0cf2748d979ae9f5eae3d5ea1e800cb7d8ddb01f
Donghyeon Min <mindonghyeon890@gmail.com>
2020-07-28 10:38:57 +0900

    다시 수정 --;

--- greeter-client/src/main/java/dev/donghyeon/greeter/GreeterClientApplication.java
+++ greeter-client/src/main/java/dev/donghyeon/greeter/GreeterClientApplication.java
@@ -5,20 +5,20 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class GreeterClientApplication implements CommandLineRunner {
-
-    @Autowired
-    private Greeter greeter;
-
-    public static void main(String[] args) {
-        SpringApplication.run(GreeterClientApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        String message = greeter.greet();
-        System.out.println(message);
-    }
-}
+//@SpringBootApplication
+//public class GreeterClientApplication implements CommandLineRunner {
+//
+//    @Autowired
+//    private Greeter greeter;
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(GreeterClientApplication.class, args);
+//    }
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        String message = greeter.greet();
+//        System.out.println(message);
+//    }
+//}
 
